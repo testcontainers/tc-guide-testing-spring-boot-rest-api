a039a00d0c550958fd6c8d4c4e99d720b0078aa5
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-12-01 17:13:50 +0530

    Apply spotless prettier code formatting

--- src/test/java/com/testcontainers/demo/CustomerControllerTest.java
+++ src/test/java/com/testcontainers/demo/CustomerControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerControllerTest {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {}
 }
